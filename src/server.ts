be6294a1927ddb063b2f62119ce6b6ae53724008
Rahmat Tomy <rahmattomyapriliyanto@gmail.com>
2026-04-01 16:24:58 +0700

    fix: init redis and web socket after run server

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,16 @@ const server = http.createServer(app);
 const port = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
-  await initRedis();
-
-  initWebSocket(server);
-
   server.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
+
+  try {
+    await initRedis();
+    initWebSocket(server);
+  } catch (err) {
+    console.error("Init error:", err);
+  }
 }
 
 bootstrap().catch((err) => {
